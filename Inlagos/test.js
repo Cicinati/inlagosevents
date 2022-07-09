7dc9393badc0844a8335e144a0ba8473cf18ffdc
Cicinati <105930963+Cicinati@users.noreply.github.com>
2022-07-09 19:35:43 +0800

    Add files via upload

---
+++ Inlagos/test.js
@@ -0,0 +1,39 @@
+/*function addSomething (num,str){
+
+var add= num + str;
+alert  (add)
+
+}
+addSomething (499," programs to write")
+
+var number1 = 7;
+var number2 = 8;
+var number1 = number2 + number1
+number1+=number2;
+number1++;
+document.write( number1);
+var food= "ape"; 
+if (food=="apple") {
+    alert( "we had fruits and it was an apple");    
+}
+else{
+alert( "no we are apes we had bannana")
+}*/
+/*var i= 1
+while ( i < 5 ) {
+    document.write( " there is always hope,")
+    i++
+}*/
+/*function betting (player, distance) {
+
+if ( distance <=350 && distance  >0 ) ;
+
+   document.write(player + " hit the ball");
+}
+
+else {
+    document.write(player + " hit home run");
+     } 
+
+        betting ("steve", 351);*/
+  
